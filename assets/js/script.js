974ea3d6cbd9b9ecf1ed90fd6e1b61c7029755b3
Matheus Junior <matheusjuniorcontato@gmail.com>
2024-03-21 16:48:30 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+
+function login() {
+    let descricao = document.getElementById('txtDescricao')
+    let curso = document.getElementById('txtCurso')
+    let erro = !validar(descricao) || !validar(curso)
+
+    document.getElementById('errorDescricao').hidden = validar(descricao)
+    document.getElementById('errorCurso').hidden = validar(curso)
+   if(!erro){
+    redireciona('index.html');
+   }
+}
+
+function redireciona(url){
+    window.location.href = url; 
+}
+
+function validar(field){
+    if  (field.value == ""){
+        return false 
+    }
+    return true 
+}
